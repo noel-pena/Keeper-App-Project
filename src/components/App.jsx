2ea2ef6d02a50840cd92006c5006ee2f5607a217
Noel Pena <127697218+noel-pena@users.noreply.github.com>
2023-12-22 19:25:24 -0500

    Add files via upload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import Header from "./Header";
-import Note from "./Note";
 import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+// function insertNote(notes){
+//   return(
+//   <Note 
+//     key={notes.id}
+//     title={notes.title}
+//     content={notes.content}
+//   />
+//   );
+// }
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(noteItem => {
+        return(
+        <Note 
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+        ) 
+      })}
       <Footer />
     </div>
   );
